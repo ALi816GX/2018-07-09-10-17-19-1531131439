6e90dec5ad4b8df01069a3809b891bce0ce842bb
linguangxiong <854501715@qq.com>
2019-07-12 09:08:48 +0800

    complete practice8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,36 @@
 package practice08;
 
 public class Klass {
+
+    private int number;
+    private String leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int num) {
+        this.number = num;
+    }
+
+    public String getDisplayName() {
+        return "Class "+ number;
+    }
+
+    public void assignLeader(Student tom) {
+        leader = tom.getName();
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,34 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+
+    private Klass klass;
+
+    public Student(int id ,String name, int age, Klass klass) {
+        super(id,name,age);
+        this.klass = klass;
+
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+
+    @Override
+    public String introduce() {
+        String result = super.introduce() + " I am a Student.";
+        if(klass!= null&&klass.getLeader()!= null&&klass.getLeader().equals(this.getName())){
+            result += " I am Leader of Class "+this.getKlass().getNumber()+".";
+        }else {
+            result +=  " I am at Class "+klass.getNumber()+".";
+        }
+        return  result;
+    }
+
 }
